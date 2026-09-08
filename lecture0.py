574825f258f5d820cb89eb62867ac41b8cef3b42
REWoodworks <RussWorley@Gmail.com>
2026-09-07 21:46:03 -0400

    new files, learned about workspace language master

---
+++ lecture0.py
@@ -0,0 +1,13 @@
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+# Create a rounded result
+z = round(x + y)
+# Print the formatted result
+print(f"{z:,}")
+
+#print(z)
+print(f"{z:,}")
+print(f"{z:,}")
+print(f"{z:,}")
+print(f"{z:,}")
