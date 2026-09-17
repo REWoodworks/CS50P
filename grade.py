91c974aa81256a094b835c3d92e7c631c8adb88d
REWoodworks <RussWorley@Gmail.com>
2026-09-16 21:43:28 -0400

    bringing desktop current

---
+++ grade.py
@@ -0,0 +1,47 @@
+####
+####
+
+### using AND with if, elif, else, and and operators to demonstrate different ways to compare two numbers and print out the result.
+### note the use of ELSE to end the pattern
+#
+# score = int(input("Score: "))
+# if score >= 90 and score <= 100:
+#     print("Grade: A")
+# elif score >=80 and score < 90:
+#     print("Grade: B")
+# elif score >=70 and score < 80:
+#     print("Grade: C")
+# elif score >=60 and score < 70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+## removes the AND operator and uses chained comparisons to demonstrate different ways to compare two numbers and print out the result.
+## tightens the code and makes it more readable. Chained comparisons are a more efficient way to compare two numbers and print out the result.
+#
+# score = int(input("Score: "))
+# if 90 <= score <= 100:
+#     print("Grade: A")
+# elif 80 <= score < 90:
+#     print("Grade: B")
+# elif 70 <= score < 80:
+#     print("Grade: C")
+# elif 60 <= score < 70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+## removes AND and chained comparisons and uses only the >= operator to demonstrate different ways to compare two numbers and print out the result.
+## final reduction, most elegant according to professor. This is the most efficient way to compare two numbers and print out the result.
+
+score = int(input("Score: "))
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
---
+++ house.py
@@ -0,0 +1,56 @@
+### Prompts a user for their name and gives their Harry Potter house based on their name.
+
+
+## Starting off using if, elif, and else statements to check the name and print out the house.
+
+name = input("What's your name? ")
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron": 
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+## Using the or operator to check if the name is one of the three Gryffindor students. 
+## This is a more efficient way to check if the name is one of the three Gryffindor students because it uses the or operator to check if the name is equal to any of the three names. 
+## If the name is equal to any of the three names, it will print "Gryffindor"; otherwise, it will print "Who?".
+
+# name = input("What's your name? ")
+# if name == "Harry" or name == "Hermione" or name == "Ron": 
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+## Match, so cool. 
+## note use of _ in final case to catch all other names.
+
+# name = input("What's your name? ")
+# match name: 
+#     case "Harry":
+#         print("Gryffindor")
+#     case "Hermione":
+#         print("Gryffindor")
+#     case "Ron": 
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin")
+#     case _:
+#         print("Who?")
+
+## Further refinment 
+## Note the match statement can use the | operator to check for multiple cases in a single line.
+
+name = input("What's your name? ")
+match name: 
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
