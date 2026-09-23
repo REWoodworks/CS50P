389f1e66bb6e44d646f86cb68adb68ed1245c77e
REWoodworks <RussWorley@Gmail.com>
2026-09-22 23:38:45 -0400

    CowSay!

---
+++ cowsay-kids.py
@@ -0,0 +1,33 @@
+# cowsay.cow('Hello World')
+# print(cowsay.get_output_string('cow', 'Hello World'))
+# cowsay.char_names
+# ['beavis', 'cheese', 'cow', 'daemon', 'dragon', 'fox', 'ghostbusters', 'kitty',
+# 'meow', 'miki', 'milk', 'octopus', 'pig', 'stegosaurus', 'stimpy', 'trex', 
+# 'turkey', 'turtle', 'tux']
+# len(cowsay.chars)
+# 19
+
+# Want to add a path to make it random, to use the dict called by 
+
+
+
+import sys
+import random
+import cowsay
+
+cow_dict = cowsay.char_names
+random_cow = random.choice(cow_dict)
+
+def main():
+    
+    if len(sys.argv) == 2:
+        cowsay.cow("Hi, " + sys.argv[1])
+    elif len(sys.argv) == 1:
+        name = input ("What is your name?").upper()
+        name = f"HI {name}"
+        print(cowsay.get_output_string(random_cow, name))
+    else:
+        sys.exit
+
+main ()
+    
